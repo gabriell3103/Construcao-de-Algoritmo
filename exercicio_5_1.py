aa9f5a0d44dcb71fa298cfed40b712be303baa87
Gabriel Silva <132635821+gabriell3103@users.noreply.github.com>
2023-09-12 22:42:52 +0000

    arquivos

---
+++ exercicio_5_1.py
@@ -0,0 +1,4 @@
+# EXERCÍCIO 5.1
+# Escreva um programa que receba uma lista de temperaturas em Celsius e escreva na tela o resultado da conversão dessas temperaturas em Fahrenheit.
+# (Não se esqueça de usar a fórmula correta!)
+
---
+++ exercicio_5_3.py
@@ -0,0 +1,2 @@
+# EXERCÍCIO 5.3
+# Reescreva o programa 5.14 validando a entrada de dados do usuário, usando a estrutura try...except.
