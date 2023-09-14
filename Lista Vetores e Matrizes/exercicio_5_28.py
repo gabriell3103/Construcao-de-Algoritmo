363af45f76591760b1f993ee684c49be3aa3d058
Gabriel Silva <132635821+gabriell3103@users.noreply.github.com>
2023-09-14 23:03:43 +0000

    finalizado

--- Lista Vetores e Matrizes/exercicio_5_28.py
+++ Lista Vetores e Matrizes/exercicio_5_28.py
@@ -1,13 +1,6 @@
 # EXERCÍCIO 5.28
 # Escreva um programa que crie uma matriz tridimensional usando o mecanismo de lista por compreensão.
 
-di_x = 3
-di_y = 3
-di_z = 3
+tridimensisonal = [[[j for j in range(i)]for i in range(3)]]
 
-matriz_3d = [[[0 for _ in range(di_z)] for _ in range(di_y)] for _ in range(di_x)]
-
-for x in range(di_x):
-    for y in range(di_y):
-        for z in range(di_z):
-            print(f"matriz_3d[{x}][{y}][{z}] = {matriz_3d[x][y][z]}")
+print(tridimensisonal)
